c09b0905f0c78c11521489a46a7a7ac7649db627
Matthew Schinckel <matt@schinckel.net>
2022-08-15 14:12:44 +0930

    Sort versions correctly

--- lib/utils.bash
+++ lib/utils.bash
@@ -32,7 +32,7 @@ platform() {
 
 sort_versions() {
   sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+    LC_ALL=C sort -n -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
 rdom() {
