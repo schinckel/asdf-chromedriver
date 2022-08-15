e322f960e527d63efc5feaa6cadfc935a9c00885
Matthew Schinckel <matt@schinckel.net>
2022-08-15 12:50:04 +0930

    Make download/install work

--- lib/utils.bash
+++ lib/utils.bash
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for chromedriver.
-GH_REPO="https://chromedriver.chromium.org"
+SOURCE="http://chromedriver.storage.googleapis.com"
 TOOL_NAME="chromedriver"
 TOOL_TEST="chromedriver --help"
 
@@ -14,26 +14,38 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if chromedriver is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
+platform() {
+  ARCH=$(uname -m)
+  PLATFORM=$(uname)
+
+  if [[ $ARCH = "arm64" ]]; then
+    NAME=chromedriver_mac64_m1
+  elif [[ $ARCH = "x86_64" ]]; then
+    if [[ $PLATFORM = "Darwin" ]]; then
+      NAME=chromedriver_mac64
+    else
+      NAME=chromedriver_linux64
+    fi
+  fi
+  echo "$NAME"
+}
 
 sort_versions() {
   sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
     LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-list_github_tags() {
-  git ls-remote --tags --refs "$GH_REPO" |
-    grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+rdom() {
+  local IFS=\>
+  read -r -d \< E C
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if chromedriver has other means of determining installable versions.
-  list_github_tags
+  curl -s "$SOURCE" | while rdom; do
+    if [[ $E = "Key" ]]; then
+      echo "$C"
+    fi
+  done | grep "$(platform)" | cut -d '/' -f 1 | sort_versions
 }
 
 download_release() {
@@ -42,7 +54,7 @@ download_release() {
   filename="$2"
 
   # TODO: Adapt the release URL convention for chromedriver
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  url="${SOURCE}/${version}/$(platform).zip"
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
